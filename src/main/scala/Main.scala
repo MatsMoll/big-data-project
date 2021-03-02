d8d5d1c652b56d7dde78fa421749c2a894752b30
Mats Eikeland Mollestad <mem@mollestad.no>
2021-03-02 15:44:38 +0100

    Updated with input_path arg support

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,15 +4,10 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 import java.util.Base64
-
-case class ProjectFileConfig(postsUri: String,
-                             commentsUri: String,
-                             usersUri: String,
-                             badgesUri: String)
-
-
+                             
 object SimpleApp {
   def main(args: Array[String]) {
+
     val spark = SparkSession.builder()
       .appName("Big Data Project")
       .master("local[1]")
@@ -20,14 +15,8 @@ object SimpleApp {
     val sc = spark.sparkContext
     sc.setLogLevel("OFF")
 
-    val config = ProjectFileConfig(
-      "data/posts.csv",
-      "data/comments.csv",
-      "data/users.csv",
-      "data/badges.csv"
-    )
+    val config = ProjectFileConfig.parseArgs(args)
     
-
     // TASK [1.1, 1.2, 1.3, 1.4]
     val badgesRdd = Setup.LoadBadgesRDD(spark, config)
     val postsRdd = Setup.LoadPostsRDD(spark, config)
---
+++ src/main/scala/ProjectFileConfig.scala
@@ -0,0 +1,35 @@
+case class ProjectFileConfig(postsUri: String,
+                             commentsUri: String,
+                             usersUri: String,
+                             badgesUri: String)
+
+import java.nio.file.{Paths, Files}
+
+object ProjectFileConfig {
+  def parseArgs(args: Array[String]): ProjectFileConfig = {
+    if (args.length != 2) {
+        throw new Exception("Missing input_path argument")
+    }
+    val inputPathIndex = args.indexWhere(arg => arg == "input_path")
+    if (inputPathIndex >= args.length) {
+        throw new Exception("Missing argument after input_path")
+    }
+    val inputPath = args(inputPathIndex + 1)
+
+    if (Files.exists(Paths.get(s"$inputPath/posts.csv"))) {
+        ProjectFileConfig(
+            s"$inputPath/posts.csv",
+            s"$inputPath/comments.csv",
+            s"$inputPath/users.csv",
+            s"$inputPath/badges.csv"
+        )
+    } else {
+        ProjectFileConfig(
+            s"$inputPath/posts.csv.gz",
+            s"$inputPath/comments.csv.gz",
+            s"$inputPath/users.csv.gz",
+            s"$inputPath/badges.csv.gz"
+        )
+    }
+  }
+}
